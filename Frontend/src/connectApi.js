2dde918bbcbc12ff0bd95a76affa7c457abe1199
amithabhghosh <amithabhghosh@gmail.com>
2025-03-07 21:27:33 +0530

    crcted code

--- Frontend/src/connectApi.js
+++ Frontend/src/connectApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL: "https://freelancebackend-gamma.vercel.app",
+    baseURL: "https://freelancebackend-gamma.vercel.app/api",
     withCredentials: true,  
 });
 
