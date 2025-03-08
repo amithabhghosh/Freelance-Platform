6a73aee1e6c28c37a76eb3d68e29e7a2fa75977b
amithabhghosh <amithabhghosh@gmail.com>
2025-03-08 18:10:32 +0530

    Removed Server.js

--- Backend/server.js
+++ Backend/server.js
@@ -32,11 +32,11 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.static(path.join(__dirname, "Frontend/dist")));
+// app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
+// });
 
 app.use((req, res, next) => {
     if (req.path === "/webhook") {
